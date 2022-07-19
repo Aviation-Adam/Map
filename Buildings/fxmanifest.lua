e30d45d4ce255415e8a22d5bc628207ec80f07d7
Aviation-Adam <98424168+Aviation-Adam@users.noreply.github.com>
2022-07-19 15:51:37 +0100

    Add files via upload

---
+++ Buildings/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+this_is_a_map 'yes'
+
+files {
+    'stream/Ruislip/paleto_bay.ymt',
+    "stream/hillingdon_props.ytyp"
+}
+
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'stream/Ruislip/paleto_bay.ymt'
+data_file 'DLC_ITYP_REQUEST' 'stream/HillingdonLAS/hillingdon_props.ytyp'
