2679448480d17b6d54a160af8b95edce258e48d7
Aviation-Adam <98424168+Aviation-Adam@users.noreply.github.com>
2022-07-19 16:07:58 +0100

    Add files via upload

---
+++ roadsigns/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+data_file 'DLC_ITYP_REQUEST' 'stream/props.ytyp'
+
+this_is_a_map 'yes'
---
+++ roadworks-pack-kaiw210/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+data_file 'DLC_ITYP_REQUEST' 'stream/bearright.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/lanesmerge.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/redlight.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/roadclosed.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/roadworks.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/trafficlights.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/bearleft.ytyp'
+
+this_is_a_map 'yes'
