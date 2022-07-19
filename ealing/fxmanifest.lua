6d1b384414ddd4cee0bb278710e6e0897f393514
Aviation-Adam <98424168+Aviation-Adam@users.noreply.github.com>
2022-07-19 16:02:39 +0100

    Add files via upload

---
+++ ealing/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ealingfirestationprops.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'v_int_1'
+
+file 'ealingfirestationprops'
+file 'v_int_1'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/westhampstead_ytyp.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/objects.ytyp'
